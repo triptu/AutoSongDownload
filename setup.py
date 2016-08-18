2c4fbb534ca14b97cfd0160a94d400f8f9e23ec6
Tushar <tushutripathi@gmail.com>
2016-08-18 12:18:19 +0530

    Added setup.py.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'AutoSongDownload',
+  packages = ['AutoSongDownload'], # this must be the same as the name above
+  version = '1.0',
+  description = 'Download songs easily in just two steps.',
+  author = 'Tushar Tripathi',
+  author_email = 'tushutripathi@gmail.com',
+  url = 'https://github.com/tushutripathi/AutoSongDownload', # use the URL to the github repo
+  download_url = 'https://github.com/tushutripathi/AutoSongDownload/tarball/1.0', # I'll explain this in a second
+  keywords = ['song', 'automatic song', 'download song'], # arbitrary keywords
+  classifiers = [],
+)
