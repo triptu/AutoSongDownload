ab48b101e5a2cf3fa3cb0b12aba204a060dd7f06
Tushar Tripathi <tushutripathi@gmail.com>
2017-03-14 21:59:20 +0530

    Updated. More features.

--- AutoSongDownload/getURL.py
+++ AutoSongDownload/getURL.py
@@ -1,21 +1,27 @@
-from youtubeAudio import download
-import requests, subprocess, os, sys, pyperclip
+import requests
+import subprocess
+import os
+import sys
+import pyperclip
 from bs4 import BeautifulSoup as bs
 from subprocess import check_output
-
+from youtubeAudio import download
 '''What it does-
 "Download song of your choice in audio format in two steps by just entering the name."
-
 
 Usage - 4 ways:-
 1. Run the program and enter the song name.
 2. From cmd line or run enter "song <song name>". This is will give you top
     n results(n depends on you).
 3. From cmd line or run enter "song /<song name>". First result will be automatically downloaded.
-4. You can just copy the song name and run "song" command from cmd or run. No
-    need to paste just copy. Note:-This will automatically download the first result by default. (You
-    can change that too.)
-5. You can create a keyboard shorcut to shorten the 4th way even more. Just copy the
+   Add '/' ultimately(or penultimately if you are using next option to not run after downloading.)
+4. Add 1 at last to download both video and audio.
+   and 0 at last to download only video.
+   Don't add anything for only audio.
+5. You can just copy the song name and run "song" command from cmd or run. No
+need to paste just copy. Note:-This will automatically download the first result by default. (You
+can change that too.)
+6. You can create a keyboard shorcut to shorten the 4th way even more. Just copy the
     the song name and press that Keyboard shortcut. And the download will start.
 
 MIT License-
@@ -23,7 +29,10 @@ Tushar Tripathi
 tushutripathi@gmail.com
 '''
 
-path='D:\\DC++Share\\Songs\\pyDirect'
+pathA = 'D:\\DC++Share\\Songs\\pyDirect'
+pathV = 'D:\\DC++Share\\Songs\\Video\\pyDirect'
+path = pathA
+
 
 # Getting pid of the cmd prompt open so that it can be closed in last.
 def get_pid():
@@ -38,130 +47,155 @@ def get_pid():
             return int(p[i].split()[1])
 
 
-def check(tag): # Custom function to get the channel names through BeautifulSoup
-    if tag.has_attr('class') and ('g-hovercard' in tag['class']) and tag.parent.name=='div':
+def check(tag):         # Custom function to get the channel names through BeautifulSoup
+    if tag.has_attr('class') and ('g-hovercard' in tag['class']) and tag.parent.name == 'div':
         return True
     else:
         return False
 
+
 # If auto is 1 it will not give options, directly send the first result.
 # Function return video url and title.
 def get(query, auto=0):
-    base="https://www.youtube.com"
+    base = "https://www.youtube.com"
     search_url = "https://www.youtube.com/results"
-    payload={'search_query':query, 'page':'', 'utm_source':'opensearch'}
-    r=requests.get(search_url, params=payload)
-    soup=bs(r.text, "html.parser")
+    payload = {'search_query': query, 'page': '', 'utm_source': 'opensearch'}
+    r = requests.get(search_url, params=payload)
+    soup = bs(r.text, "html.parser")
+    # all a tags containing link has this in class name
+    videos = soup.find_all("a", class_="yt-ui-ellipsis-2")
 
-    videos=soup.find_all("a", class_="yt-ui-ellipsis-2") #all a tags containing link
-                                                         #has this in class name
-
-    if auto==1:     # Automatically choses the first one to download.
-        print ("File to download ",videos[0].string)
+    if auto == 1:     # Automatically choses the first one to download.
+        print ("File to download ", videos[0].string)
         return (base + videos[0]['href'], videos[0]['title'])
 
     else:
-        count,cc=1,0    # cc=Channel Count-wiil be used to access the channel name
-                                # from the channels array
-        channels=soup.find_all(check) #Scraping channel names
+        # cc=Channel Count-will be used to access the channel name from the channels array.
+        count, cc = 1, 0
+        channels = soup.find_all(check)  # Scraping channel names
         print('\a')
         print ("Choose what you want. Just enter the corresponding number.")
 
         for video in videos:
-            if 'g-hovercard' in video['class']: # Skipping over channels
-                cc+=1
+            if 'g-hovercard' in video['class']:  # Skipping over channels
+                cc += 1
                 continue
             try:
-                print "%d. %s" %(count, video.string), # In case of playlist the code
-                                                                            # will tell in the same line
+                # In case of playlist the code will tell in the same line
+                print "%d. %s" % (count, video.string.strip()),
                 if 'list' in video['href']:    # If its a playlist
                     print " - A PLAYLIST",
                 print ""
 
-                channel=channels[cc]
-                if video.parent.next_sibling.string=="YouTube":
+                channel = channels[cc]
+                if video.parent.next_sibling.string == "YouTube":
                     print ("-Made by Youtube.")
                 else:
                     try:        # Checking if its verified channel
-                        p=channel.next_sibling.next_sibling
-                        print "Channel - ", channel.string,"-Verified"
+                        print "Channel - ", channel.string.strip(), "-Verified"
                     except:
-                        print "Channel - ", channel.string  # Not verified
-                    count+=1
+                        print "Channel - ", channel.string.strip()  # Not verified
+                    count += 1
 
-                cc+=1
+                cc += 1
 
             except:
                 pass
 
-            if count==7 or cc==len(channels): #Change it to how many options you want(max value-21)
+            # Change it to how many options you want(max value-21)
+            if count == 6 or cc == len(channels):
                 break
             print ""
-        choice=int(raw_input("\nNow enter your choice- "))
-        print "You chose- ", videos[choice-1].string
+        choice = int(raw_input("\nNow enter your choice- "))
+        print "You chose- ", videos[choice - 1].string.encode('ascii', 'ignore')
         print "Download is starting..."
-        return (base + videos[choice-1]['href'], videos[choice-1]['title'])
+        return (base + videos[choice - 1]['href'], videos[choice - 1]['title'])
 
 
 def openFile(title):
-    #print "Opening the folder."
-    #subprocess.Popen('explorer '+path)
+    # print "Opening the folder."
+    # subprocess.Popen('explorer '+path)
     global path
     print "Let's play the music."
-    for ext in ['.webm', '.m4a', '.mp3']:
-        name=path+"\\"+title+ext
+    for ext in ['.webm', '.m4a', '.mp3', '.mp4', '.flv']:
+        name = path + "\\" + title + ext
         if os.path.isfile(name):
-            subprocess.Popen('"'+name+'"', shell=True)
+            subprocess.Popen('"' + name + '"', shell=True)
             break
-        elif os.path.isfile(name.replace('-','_')):  # In hindi songs - is replaced by _ for some weird reasons.
-            name = name.replace('-','_')
-            subprocess.Popen('"'+name+'"', shell=True)
+        # In hindi songs - is replaced by _ for some reasons IDK. Smthing to do with youtube-dl.
+        elif os.path.isfile(name.replace('-', '_')):
+            name = name.replace('-', '_')
+            subprocess.Popen('"' + name + '"', shell=True)
             break
     else:
         print "File can't be opened for some reasons."
         print "Opening the folder."
-        path = '"'+path+'"'
-        subprocess.Popen('explorer '+path)
+        path = '"' + path + '"'
+        subprocess.Popen('explorer ' + path)
 
 
 def main(query):
+    global path
+    whatToDownload = 'audio'                    # Whether to download audio or video.
+    if query[-1] == '1':            # Download Video.
+        print "Note:- Song video as well as audio will be downloaded."
+        query = query[:-1].strip()
+        path = pathV
+        whatToDownload = 'both'
+    elif query[-1] == '0':
+        query = query[:-1].strip()
+        path = pathV
+        whatToDownload = 'video'
+        print "Only Song video will be downloaded."
+    else:
+        print "Only Song Audio will be downloaded."
 
-    file_open = True # Whether to open the file after download
-    if query[-1]=='/':
+    file_open = True                # Whether to open the file after download.
+    if query[-1] == '/':              # Do not open the file after download.
         query = query[:-1]
         file_open = False
 
-    if query[0]=='/':
+    if query[0] == '/':               # Download the first song without showing any list.
         query = query[1:]
-        url,title = get(query,1)
-    else:
-        url,title=get(query)
+        url, title = get(query, 1)
+    else:                           # Show the list to chose what to download.
+        url, title = get(query)
 
-    title = title.replace('"', "'") # Replacing " by ' . Because youtube_dl does so before saving.
+    title = title.encode('ascii', 'ignore')
+    title = title.replace('"', "'")  # Replacing " by '. Because youtube_dl does so before saving.
 
-    print "Got the url",url
+    print "Got the url", url
     print "And the name", title
-    for ext in ['.webm', '.m4a', '.mp3']:
-        name = path+"\\"+title+ext
-        if os.path.isfile(name) or os.path.isfile(name.replace('_','-')):
-            print "But it's already downloaded."
+    for ext in ['.webm', '.m4a', '.mp3', '.mp4', '.flv']:
+        name = path + "\\" + title + ext
+        if os.path.isfile(name) or os.path.isfile(name.replace('_', '-')):
+            print "Oh! It's already downloaded."
             break
     else:
         print "Starting download..."
-        download(url, title)
+        if whatToDownload in ['audio', 'both']:
+            print "Downloading Audio..."
+            download(url, title, audio=True)
+        if whatToDownload in ['video', 'both']:
+            print "\nNow Downloading Video..."
+            download(url, title, audio=False)
     if file_open:
         openFile(title)
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     pid = get_pid()
     try:
         sys.argv[1]         # Checks if cmd line argument is given
-        query= " ".join(sys.argv[1:])
+        query = " ".join(sys.argv[1:])
     except:
         if "idlelib" in sys.modules:
             query = raw_input("Enter the song's name. - ")
         else:
-            query = "/"+pyperclip.paste()
-    main(query)
-    subprocess.Popen("TASKKILL /F /PID {pid}".format(pid=pid))
+            query = "/" + pyperclip.paste()
+    try:
+        main(query)
+        subprocess.Popen("TASKKILL /F /PID {pid}".format(pid=pid))  # Close cmd only if successful.
+    except Exception as e:
+        print "Error occured-"
+        print e
--- AutoSongDownload/youtubeAudio.py
+++ AutoSongDownload/youtubeAudio.py
@@ -1,17 +1,30 @@
 from __future__ import unicode_literals
 import youtube_dl, os, subprocess
 
-def download(url, name):
-    path='D:/DC++Share/Songs/pyDirect'
+pathA = 'D:/DC++Share/Songs/pyDirect'
+pathV = 'D:/DC++Share/Songs/Video/pyDirect'
+
+def download(url, name, audio=True):
+    if audio==True:   # To download audio.
+        path = pathA
+    else:
+        path = pathV
     print "Downloading-", name
-    os.chdir(path)
-    options = {
-        'format': 'bestaudio/best', # choice of quality
-        'extractaudio' : True,      # only keep the audio
-        'audioformat' : "mp3",      # convert to mp3
-        'outtmpl' : '%(title)s.%(ext)s',
-        'noplaylist' : True,        # only download single song, not playlist
-    }
+    os.chdir(path)                          # Path changed to download folder.
+    if audio:
+        options = {
+            'format': 'bestaudio/best',         # choice of quality
+            'extractaudio' : True,             # choosing whether to donwload audio or video.
+            'audioformat' : "mp3",              # convert to mp3 for audio format.
+            'outtmpl' : '%(title)s.%(ext)s',    # Name of output.
+            'noplaylist' : True,                # only download single song, not playlist
+        }
+    else:
+        options = {
+            'outtmpl' : '%(title)s.%(ext)s',    # Name of output.
+            'noplaylist' : True,                # only download single song, not playlist
+        }
+
     ydl = youtube_dl.YoutubeDL(options)
     with ydl:
         ydl.download([url])
